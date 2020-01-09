df947739ba8560906baed02103c7108d2ce1e0f3
anil gurung <anil_grg@hotmail.com>
2020-01-09 21:17:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,9 @@
-class 
+class Shoe
+  attr_accessor :brand,:color,:size,:material,:condition
+  def initializer (brand,color,size,material,condition)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
